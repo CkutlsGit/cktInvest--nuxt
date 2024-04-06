c8104168cdd5a44d3c926973d1439c74a27b773b
Kostya <ckutls228@gmail.com>
2024-04-06 12:16:38 +0300

    added style for main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <h1 class="flex justify-center">123</h1>
-  </div>
+  <main class="main mt-12">
+    <div class="main__content font-custom-poppins container flex flex-col items-center mx-auto">
+      <h1 class="max-w-screen-md text-3xl text-color-beige-custom-200 font-semibold text-center">
+        <strong>CKT</strong>Invest - простой сервис для отслеживания котировок бумаг компаний СПм
+      </h1>
+      <p class="max-w-screen-md mt-2 text-center text-sm text-gray-400 font-semibold">
+        Наш сервис предоставляет информацию о котировках каждой СПм компании и никак не предоставляет это как рекомендацию.
+      </p>
+    </div>
+  </main>
 </template>
 
 <script setup>

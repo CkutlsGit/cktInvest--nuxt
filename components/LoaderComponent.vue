05bf405b4e097146ad072e7772867754938389ef
Kostya <ckutls228@gmail.com>
2024-04-06 13:57:53 +0300

    added loader for securites-block

---
+++ components/LoaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="spinner"></div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+ .spinner {
+   width: 56px;
+   height: 56px;
+   border-radius: 50%;
+   border: 9px solid;
+   border-color: #dbdcef;
+   border-right-color: #41844b;
+   animation: spinner-d3wgkg 1s infinite linear;
+ }
+
+@keyframes spinner-d3wgkg {
+  to {
+    transform: rotate(1turn);
+  }
+}
+</style>
--- components/PopularSecuritesComponent.vue
+++ components/PopularSecuritesComponent.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="securites-block mt-6">
     <div class="securites-block__content bg-gray-500 rounded-2xl w-96 p-6 max-sm:w-40">
-      <div class="element text-white text-xl">
+      <div v-if="!statusClaimData" class="relative left-36">
+        <LoaderComponent/>
+      </div>
+      <div v-else class="element text-white text-xl">
         <ul class="element__content max-w-full mx-auto flex flex-col">
           <li v-for="info in popularSecurites" :key="info.id">
             <NuxtLink to="/markets" class="flex justify-between cursor-pointer max-sm:flex-col max-sm:text-center max-sm:mt-4">
@@ -18,9 +21,14 @@
 
 <script setup>
   let popularSecurites = reactive({})
+  const statusClaimData = ref(false)
 
   onMounted(async () => {
     const data = await $fetch('/api/securites/getpopularsecurites')
+
+    if (data) {
+      statusClaimData.value = true
+    }
 
     Object.assign(popularSecurites, data)
   })

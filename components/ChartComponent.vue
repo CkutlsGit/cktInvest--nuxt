090764d9cac37302684539fc2600529014686785
Kostya <ckutls228@gmail.com>
2024-04-07 09:43:28 +0300

    changed language for assets.json, added some styles, added graph without functional

---
+++ components/ChartComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="chart-content">
+    <LineChart style="height: 12rem; width: 12rem" v-bind="lineChartProps" :options="options"></LineChart>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js';
+import {LineChart, useLineChart} from "vue-chart-3";
+Chart.register(...registerables)
+
+export default {
+
+  components: { LineChart },
+  setup(props) {
+    const data = reactive([120, 100, 40, 0, 80, 160, 120])
+    const options = reactive({
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    })
+
+    const chartData = computed(() => ({
+      labels: [],
+      datasets: [{
+        data: data,
+        backgroundColor: '#8021F9',
+        borderColor: '#9560EB'
+      }],
+    }))
+
+    const { lineChartProps, lineChartRef } = useLineChart({
+      chartData
+    })
+
+    return { options, lineChartProps, lineChartRef }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/markets/[id].vue
+++ pages/markets/[id].vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="active-info">
-    <div class="active-info__content">
+  <div class="active-info mt-16">
+    <div class="active-info__content bg-gray-500 text-white font-medium font-custom-poppins rounded-2xl container mx-auto min-h-96 p-6">
       <div v-if="!statusClaimData">
         <LoaderComponent/>
       </div>
       <div v-else class="info-block">
-        <h1>123123</h1>
+        <h1>{{ activeInfo.name }}</h1>
+        <p>{{ activeInfo.description }}</p>
+        <div class="info-count-precent mt-2 flex items-center">
+          <h2>{{ activeInfo.price.start }} | {{ activeInfo.price.end }}</h2>
+          <h2 class="ml-2" :class="GetColor(activeInfo.pricehistory[0], activeInfo.price.start)">{{ GetPrecent(activeInfo.pricehistory[0], activeInfo.price.start) }}%</h2>
+        </div>
+        <div class="news-block mt-6">
+          <h1>Новости компании</h1>
+          <ul class="max-h-56 overflow-y-auto">
+            <li class="mt-6" v-for="(news, index) in activeInfo.news" :key="index">
+              <h1 class="mb-1">{{ news.title }}</h1>
+              <p class="mb-2">{{ news.description }}</p>
+              <h2>{{ index }}</h2>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
+  <ChartComponent/>
 </template>
 
+<!--<h1 :class="GetColor(active.pricehistory[0], active.price.start)">{{ GetPrecent(active.pricehistory[0], active.price.start) }}%</h1>-->
 <script setup>
-  //
+  import GetPrecent from "~/utils/getPrecent.js";
+  import GetColor from "~/utils/getColor.js";
+  import ChartComponent from "~/components/ChartComponent.vue";
+
+  let activeInfo = reactive({})
   const statusClaimData = ref(false)
   const route = useRoute()
 
@@ -24,8 +45,13 @@
       }
     })
 
+    console.log(data)
+
     if (data) {
       statusClaimData.value = true
+
+      Object.assign(activeInfo, data)
+      console.log(true)
     }
     else {
       statusClaimData.value = true
